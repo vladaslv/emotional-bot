4eb67d894d0add7eb2446e9068708b10ebc953af
vldsl <vladyslava.solovei@gmail.com>
2020-06-05 02:55:11 +0200

    initialize bot responses

--- const.py
+++ const.py
@@ -1,8 +1,28 @@
-NEGATIVE_EMOTIONS = ['anger', 'sadness', 'fear']
-POSITIVE_EMOTIONS = ['joy']
-MESSAGES = {
-    -1: ['bla1', 'bla2', 'bla3', 'bla4', 'bla5'],
-    0: ['n1', 'n2', 'n3', 'n4', 'n5'],
-    1: ['pos1', 'pos2', 'pos3', 'pos4', 'pos5']
+MOODS={
+    -1:'negative',
+    0:'neutral',
+    1:'positive',
 }
-GRAPH_URL = 'https://graph.facebook.com/v7.0/me/messages?access_token='
+NEGATIVE_EMOTIONS=['anger', 'sadness', 'fear']
+POSITIVE_EMOTIONS=['joy']
+MESSAGES={
+    -1: [
+        'It hurts my feelings',
+        'I feel so bad about what you wrote',
+        'Your message make me sad',
+        'I\'m very sorry to hear that',
+        'Sorry, things aren\'t working for you'],
+    0: [
+        'I like your way of thinking',
+        'I understand',
+        'I see',
+        'Interesting, Ok',
+        'That\'s Ok'],
+    1: [
+        'Fine, tell me more!',
+        'Wow, sounds great!',
+        'That\'s good news!',
+        'Oh, that\'s very kind of you',
+        'That\'s wonderful! Tell me everything from start to finish!']
+}
+GRAPH_URL='https://graph.facebook.com/v7.0/me/messages?access_token='
